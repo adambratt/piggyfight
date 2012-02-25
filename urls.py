fb1e3ea3b5a5e2f42292ed9ef6ba2967573d47ae
Adam Bratt <adamjbratt@gmail.com>
2012-02-25 17:35:37 -0500

    added structure for game

--- urls.py
+++ urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^members/login/$', 'django.contrib.auth.views.login', {'template_name': 'members/login.html'}),
     url(r'^members/logout/$', 'members.views.do_logout'),
     # Image serving
+    url(r'^image/(?P<image_id>\w+)/$', 'images.views.load'),
     url(r'^image/(?P<image_id>\w+)/(?P<width>\w+)/(?P<height>\w+)/$', 'images.views.load'),
     # Member Views
     url(r'^dashboard/$', 'members.views.dashboard'),
