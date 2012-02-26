881cbba23f4de799fbc6daeb31c02eccc78a0576
Adam Bratt <adamjbratt@gmail.com>
2012-02-26 13:34:36 -0500

    quick test

--- settings.py
+++ settings.py
@@ -162,12 +162,12 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers':['console'],
+            'handlers':['console', 'logfile'],
             'propagate': True,
             'level':'WARN',
         },
         'django.db.backends': {
-            'handlers': ['console'],
+            'handlers': ['console', 'logfile'],
             'level': 'DEBUG',
             'propagate': False,
         },
