b338c12c1fb7ccfe7817c0b6537119e394f25b27
Adam Bratt <adamjbratt@gmail.com>
2012-02-26 11:56:09 -0500

    something has to be wrong

--- game/views.py
+++ game/views.py
@@ -1,5 +1,8 @@
-# Create your views here.
-
+from twilio.twiml import Response
+from django_twilio.decorators import twilio_view
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from game.models import Post
 
 def rules(request):
     return
@@ -18,3 +21,38 @@ def join_group(request, group_id):
 
 def create_group(request):
     return
+
+@require_POST
+def mailgun(request):
+    sender    = request.POST.get('sender')
+    subject   = request.POST.get('subject', '')
+    body = request.POST.get('body-plain', '')
+
+    # attachments:
+    for key in request.FILES:
+        file = request.FILES[key]
+        form=PhotoForm(request.POST, { key: file })
+        if form.is_valid():
+            newphoto=form.save()
+            newphoto.member = request.user.get_profile()
+            post=Post(newphoto)
+            post.member = request.user.get_profile()
+            post.save()
+            
+    # Returned text is ignored but HTTP status code matters:
+    return HttpResponse('OK')
+
+@twilio_view
+def twilio(request):
+    r = Response()
+    r.sms('Your message has been received and will be verified shortly!')
+    return r
+
+
+#### Functions ####
+
+def handle_upload(file):
+    destination = open('some/file/name.txt', 'wb+')
+    for chunk in f.chunks():
+        destination.write(chunk)
+    destination.close()
