b6a134599163c4941acf2e945672e1b30ae3e1eb
Adam Bratt <adamjbratt@gmail.com>
2012-02-26 14:42:00 -0500

    works

--- members/views.py
+++ members/views.py
@@ -3,6 +3,7 @@ from members.forms import PhotoForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from members.forms import RegistrationForm
+from django.contrib.auth.models import User
 
 ##### Views #####
 
