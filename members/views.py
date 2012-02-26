bbcc6dd5fc7fe26c3c538c64c13cc3393626e926
Adam Bratt <adamjbratt@gmail.com>
2012-02-26 14:29:00 -0500

    regform

--- members/views.py
+++ members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from members.forms import PhotoForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from members.forms import RegistrationForm
 
 ##### Views #####
 
